ee8922ad57dc28bb603a6910206681042ea356f7
Wizer27 <svd2yar@gmail.com>
2026-03-19 16:57:23 +0300

    api key secrity added

--- api/main.go
+++ api/main.go
@@ -9,6 +9,22 @@ type UserData struct {
 	UserId int `json:"user_id"`
 }
 
+func auth_middleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		token := c.GetHeader("Authorization")
+
+		if token != "secret" {
+			c.AbortWithStatusJSON(401, gin.H{
+				"error": "unathorized",
+			})
+			return
+		}
+
+		c.Next()
+
+	}
+}
+
 func generate_jwt_token(user_id int) string {
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
 		"user_id": user_id,
@@ -26,7 +42,7 @@ func main() {
 		})
 	})
 
-	r.POST("/get_token", func(c *gin.Context) {
+	r.POST("/get_token", auth_middleware(), func(c *gin.Context) {
 
 		var user_data UserData
 
